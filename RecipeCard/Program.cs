acf5b69d9511e03c6173b9f4f63674ea78d9a0bc
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-05 13:56:23 +0900

    RecipeCard 완료

--- RecipeCard/Program.cs
+++ RecipeCard/Program.cs
@@ -1,4 +1,17 @@
+using RecipeCard;
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Recipe recipe = new Recipe("토마토 파스타", 2, new Ingredient[] {
+    new Ingredient("스파게티면", 200, "g"),
+    new Ingredient("토마토소스", 150, "ml"),
+    new Ingredient("양파", 1, "개"),
+    new Ingredient("마늘", 3, "쪽")
+});
+
+Console.WriteLine($"=== 원본 레시피 ===");
+recipe.PrintRecipe();
+Console.WriteLine();
+
+Console.WriteLine($"=== 4인분으로 변환 ===");
+recipe.ScaleRecipe(4).PrintRecipe();
+Console.WriteLine();
---
+++ RecipeCard/Recipe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RecipeCard {
+    internal struct Recipe {
+        public string Name;
+        public int Servings;
+        public Ingredient[] Ingredients;
+
+        public Recipe(string name, int servings, Ingredient[] ingredients) {
+            Name = name;
+            Servings = servings;
+            Ingredients = ingredients;
+        }
+
+        public void PrintRecipe() {
+            Console.WriteLine($"[{Name}] ({Servings}인분)");
+            Console.WriteLine($"재료 : ");
+            foreach (var ingre in Ingredients) {
+                Console.WriteLine($"- {ingre.Name} : {ingre.Amount}{ingre.Unit}");
+            }
+        }
+        public Recipe ScaleRecipe(int newServings) {
+            Ingredient[] newIngredients = new Ingredient[Ingredients.Length];
+            
+            for (int i = 0; i < Ingredients.Length; i++) {
+                newIngredients[i] = new Ingredient($"{Ingredients[i].Name}", Ingredients[i].Amount / Servings * newServings, Ingredients[i].Unit);
+            }
+
+            return new Recipe(Name, newServings, newIngredients);
+        }
+    }
+}
