d5975f0129eb495452b7bd9743406b0d24237ce2
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-05 13:56:16 +0900

    InventoryItem 완료

---
+++ InventoryItem/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryItem {
+    internal struct Item {
+        public string Name;
+        public double Weight;
+        public int Price;
+
+        public Item(string name, double weight, int price) {
+            Name = name;
+            Weight = weight;
+            Price = price;
+        }
+    }
+}
--- InventoryItem/Program.cs
+++ InventoryItem/Program.cs
@@ -1,4 +1,48 @@
+using InventoryItem;
 using System;
+using System.Threading.Channels;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Item[] items = new Item[] {
+    new Item("검", 3.5, 1500),
+    new Item("방패", 5.0, 1200),
+    new Item("포션", 0.3, 50),
+    new Item("활", 1.8, 1300),
+    new Item("투구", 2.0, 800),
+};
+
+Console.WriteLine($"=== 인벤토리 목록 ===");
+int count = 0;
+double weightSum = 0;
+int priceSum = 0;
+
+int highestPrice = int.MinValue;
+string maxPriceName = null;
+
+double minWeight = double.MaxValue;
+string minWeightName = null;
+
+foreach (Item item in items) {
+    Console.WriteLine($"{item.Name} - 무게 : {item.Weight}kg, 가격 : {item.Price}골드");
+    count++;
+    weightSum += item.Weight;
+    priceSum += item.Price;
+
+    if (minWeight > item.Weight) {
+        minWeightName = item.Name;
+        minWeight = item.Weight;
+    }
+
+    if (highestPrice <  item.Price) {
+        maxPriceName = item.Name;
+        highestPrice = item.Price;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"=== 인벤토리 통계 ===");
+Console.WriteLine($"전체 무게 : {weightSum:F1}kg");
+Console.WriteLine($"전체 가격 : {priceSum:N0}골드");
+Console.WriteLine($"평균 가격 : {priceSum / count:N0}골드");
+Console.WriteLine($"가장 비싼 아이템 : {maxPriceName}");
+Console.WriteLine($"가장 가벼운 아아템 : {minWeightName}");
+Console.WriteLine();
+Console.WriteLine();
